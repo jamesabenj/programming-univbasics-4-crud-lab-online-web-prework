4a763daf4bf8ed823a8c8763f13a4ec4ebf7ca5a
James Benjamin <james.abenj@gmail.com>
2019-09-06 03:49:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,47 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ ["i", "am", "learning", "code", ]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array = ["i", "am", "learning", "code"]
+ element = "today"
+ add_element_to_end_of_array.push(element)
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+array = create_an_array
+create_an_array.unshift(element)
+
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = create_an_array
+    create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = create_an_array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = create_an_array
+  index_number = create_an_array[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = create_an_array
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = create_an_array
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
