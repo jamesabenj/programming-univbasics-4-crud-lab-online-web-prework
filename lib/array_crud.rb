345d060005c58fb3383d4caa1bf5977475fbaff6
James Benjamin <james.abenj@gmail.com>
2019-09-06 03:55:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   array = ["i", "am", "learning", "code"]
+   array = ["wow", "i", "am", "learning", "code"]
    array.shift
 end
 
