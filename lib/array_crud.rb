4089dedcdd7616f4b3b4add39e3a10eab47db82c
James Benjamin <james.abenj@gmail.com>
2019-09-06 04:00:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,7 +35,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["i", "am", "learning", "code"]
+  array = ["wow", "i", "am", "learning", "code"]
   array[0]
 end
 
