00f356e4aa286b05074acfa2df0a334ae5624a1a
James Benjamin <james.abenj@gmail.com>
2019-09-06 03:58:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,8 +30,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = create_an_array
-  index_number = create_an_array[]
+  array = ["i", "am", "learning", "code"]
+  index_number = array[2]
 end
 
 def retrieve_first_element_from_array(array)
